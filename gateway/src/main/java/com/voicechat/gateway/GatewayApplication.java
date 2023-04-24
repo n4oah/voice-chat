815ad70b17daa84b79b86f38ada24f12282724ca
n4oah <n4oahdev@gmail.com>
2023-04-24 20:16:42 +0900

    init project

---
+++ gateway/src/main/java/com/voicechat/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.voicechat.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
