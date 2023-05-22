f09917f314e2555cf0fe69e490a601157002b67d
n4oah <n4oahdev@gmail.com>
2023-05-22 23:44:22 +0900

    feat: Global exception handler

--- channel-invite/src/main/java/com/voicechat/channelinvite/ChannelInviteApplication.java
+++ channel-invite/src/main/java/com/voicechat/channelinvite/ChannelInviteApplication.java
@@ -1,11 +1,14 @@
 package com.voicechat.channelinvite;
 
+import com.voicechat.common.error.GlobalHttpExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableFeignClients
+@Import(GlobalHttpExceptionHandler.class)
 public class ChannelInviteApplication {
 
 	public static void main(String[] args) {
--- channel/src/main/java/com/voicechat/channel/ChannelApplication.java
+++ channel/src/main/java/com/voicechat/channel/ChannelApplication.java
@@ -1,9 +1,12 @@
 package com.voicechat.channel;
 
+import com.voicechat.common.error.GlobalHttpExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(GlobalHttpExceptionHandler.class)
 public class ChannelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChannelApplication.class, args);
