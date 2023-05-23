1f046808a2275f5845ee6b2cb2e4e659f07c97ba
n4oah <n4oahdev@gmail.com>
2023-05-24 00:30:53 +0900

    f

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,23 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-return (
-<Html lang="en">
-<Head />
-<body>
-<Main />
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
 
-
-
-
-
-
-
-
-
-{/* /////// */}
-<NextScript />
-</body>
-</Html>
-)
+        {/* /////// */}
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
