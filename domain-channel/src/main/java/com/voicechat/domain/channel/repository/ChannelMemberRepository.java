6db19cd62ac28dc70dd90137b8ba3929f195e913
n4oah <n4oahdev@gmail.com>
2023-05-28 19:17:25 +0900

    feat: userId로 channelMembers 뽑아오기

--- domain-channel/src/main/java/com/voicechat/domain/channel/repository/ChannelMemberRepository.java
+++ domain-channel/src/main/java/com/voicechat/domain/channel/repository/ChannelMemberRepository.java
@@ -4,9 +4,11 @@ import com.voicechat.domain.channel.entity.Channel;
 import com.voicechat.domain.channel.entity.ChannelMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChannelMemberRepository extends JpaRepository<ChannelMember, Long> {
     Long countByChannel(Channel channel);
     Optional<ChannelMember> findByChannelAndUserId(Channel channel, Long userId);
+    List<ChannelMember> findByUserId(Long userId);
 }
