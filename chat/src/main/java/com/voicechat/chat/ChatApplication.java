d81e982781705ca61221cbe8ff642d09a75dd5fd
n4oah <n4oahdev@gmail.com>
2023-06-27 23:10:27 +0900

    fix: EnableMongoAuditing

--- chat/src/main/java/com/voicechat/chat/ChatApplication.java
+++ chat/src/main/java/com/voicechat/chat/ChatApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableMongoAuditing
 @EntityScan("com.voicechat.domain.chat.entity")
 @EnableMongoRepositories("com.voicechat.domain.chat.repository")
 public class ChatApplication {
